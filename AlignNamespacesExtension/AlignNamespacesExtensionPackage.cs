c6b6c844bbf9a624b69525d2eb1f794f68f556f8
Matt Lantz <mattl@herbalife.com>
2020-12-23 12:10:55 -0800

    Fires for solution

--- AlignNamespacesExtension/AlignNamespacesExtensionPackage.cs
+++ AlignNamespacesExtension/AlignNamespacesExtensionPackage.cs
@@ -61,12 +61,13 @@ namespace AlignNamespacesExtension
             UnityContainer.RegisterType<IFileLoader, FileLoader>();
             UnityContainer.RegisterType<IAlignNamespacesService, AlignNamespacesService>();
             UnityContainer.RegisterType<IAlignNamespacesCommandService, AlignNamespacesCommandService>();
-
+            
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             await AlignNamespacesInProjectCommand.InitializeAsync(this);
             await AlignNamespacesInFolderCommand.InitializeAsync(this);
+            await AlignNamespacesInSolutuionCommand.InitializeAsync(this);
         }
 
         #endregion
